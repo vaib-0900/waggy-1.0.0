51716e7d652bb989d0f05d3015f090e15cfeac0c
= <=>
2025-05-27 11:19:10 +0530

    pint_invoice.php some changes

--- print_invoice.php
+++ print_invoice.php
@@ -281,7 +281,9 @@
                     </a>
                     
 
-                 
+                    <a href="order_history.php" class="btn btn-outline-secondary ms-2">
+                        <i class="fas fa-arrow-left me-2"></i> Back to Orders
+                    </a>
                 </div>
             </div>
         </div>
